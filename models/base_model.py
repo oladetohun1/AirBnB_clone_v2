97f9dcff8a71ac7682cc4f45aef2ce7a27bdf76c
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-17 14:01:58 +0200

    Commit

--- models/base_model.py
+++ models/base_model.py
@@ -32,9 +32,6 @@ class BaseModel:
 
             self.id = str(uuid.uuid4())
 
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
-
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
