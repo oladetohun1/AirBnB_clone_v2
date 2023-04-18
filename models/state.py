0e62294c1eeb0d83b1c2829adf50fdcd8af281de
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-18 12:25:12 +0200

    Commit

--- models/state.py
+++ models/state.py
@@ -14,7 +14,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
 
-    if (getenv("HBNB_TYPE_STORAGE") != "db"):
+    if (getenv("HBNB_TYPE_STORAGE") == "FileStorage"):
         @property
         def cities(self):
             """Get a list of all cities related to State object"""
