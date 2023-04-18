c90fba956a3ea964f3b4ac0c71821562d72ba1b9
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-18 12:28:50 +0200

    Commit

--- models/__init__.py
+++ models/__init__.py
@@ -6,7 +6,7 @@ from os import getenv
 if (getenv("HBNB_TYPE_STORAGE") == "db"):
     from .engine.db_storage import DBStorage
     storage = DBStorage()
-else:
+elif (getenv("HBNB_TYPE_STORAGE") == "FileStorage"):
     from .engine.file_storage import FileStorage
     storage = FileStorage()
 
