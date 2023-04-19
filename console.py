1d2468d784ea047219c15b14c841ddbe59a58cfb
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-19 04:42:18 +0200

    Commit

--- console.py
+++ console.py
@@ -257,10 +257,9 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, args):
         """ Updates a certain object with new info """
-        c_name = c_id = att_name = att_val = kwargs = ''
 
         # isolate cls from id/args, ex: (<cls>, delim, <id/args>)
-        args = args.partition(" ")
+        args = args.split(" ")
         if args[0]:
             c_name = args[0]
         else:  # class name not present
@@ -269,74 +268,39 @@ class HBNBCommand(cmd.Cmd):
         if c_name not in HBNBCommand.classes:  # class name invalid
             print("** class doesn't exist **")
             return
-
         # isolate id from args
-        args = args[2].partition(" ")
-        if args[0]:
-            c_id = args[0]
-        else:  # id not present
+        if len(args) < 2:  # id not present
             print("** instance id missing **")
             return
 
-        # generate key from class and id
+        c_id = args[1]
         key = c_name + "." + c_id
-
-        # determine if key is present
         if key not in storage.all():
             print("** no instance found **")
             return
 
-        # first determine if kwargs or args
-        if '{' in args[2] and '}' in args[2] and type(eval(args[2])) is dict:
-            kwargs = eval(args[2])
-            args = []  # reformat kwargs into list, ex: [<name>, <value>, ...]
-            for k, v in kwargs.items():
-                args.append(k)
-                args.append(v)
-        else:  # isolate args
-            args = args[2]
-            if args and args[0] == '\"':  # check for quoted arg
-                second_quote = args.find('\"', 1)
-                att_name = args[1:second_quote]
-                args = args[second_quote + 1:]
-
-            args = args.partition(' ')
-
-            # if att_name was not quoted arg
-            if not att_name and args[0] != ' ':
-                att_name = args[0]
-            # check for quoted val arg
-            if args[2] and args[2][0] == '\"':
-                att_val = args[2][1:args[2].find('\"', 1)]
-
-            # if att_val was not quoted arg
-            if not att_val and args[2]:
-                att_val = args[2].partition(' ')[0]
-
-            args = [att_name, att_val]
-
-        # retrieve dictionary of current objects
         new_dict = storage.all()[key]
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
 
-        # iterate through attr names and values
-        for i, att_name in enumerate(args):
-            # block only runs on even iterations
-            if (i % 2 == 0):
-                att_val = args[i + 1]  # following item is value
-                if not att_name:  # check for att_name
-                    print("** attribute name missing **")
-                    return
-                if not att_val:  # check for att_value
-                    print("** value missing **")
-                    return
-                # type cast as necessary
-                if att_name in HBNBCommand.types:
-                    att_val = HBNBCommand.types[att_name](att_val)
+        att_name = args[2]
+        if len(args) < 4:
+            print("*** value missing **")
+            return
+        att_val = args[3]
 
-                # update dictionary with name, value pair
-                new_dict.__dict__.update({att_name: att_val})
+        # validate attribute
+        if att_name in HBNBCommand.types:
+            try:
+                att_val = HBNBCommand.types[att_name](att_val)
+            except ValueError:
+                print("** invalid value for attribute {} **".format(att_name))
+                return
 
-        new_dict.save()  # save updates to file
+        # Update dictionary with namr, value
+        setattr(new_dict, att_name, att_val)
+        new_dict.save()
 
     def help_update(self):
         """ Help information for the update class """
