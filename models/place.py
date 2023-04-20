6d5ff2d8a47d80c89ce9d1dbccbb7370323a3979
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-20 18:47:58 +0200

    Commit

--- models/place.py
+++ models/place.py
@@ -8,16 +8,17 @@ from models.review import Review
 from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
 
 
-association_table = Table("place_amenity", Base.metadata,
-                          Column("place_id",
-                                 String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True,
-                                 nullable=False),
-                          Column("amenity_id", String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True,
-                                 nullable=False))
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id",
+                             String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
