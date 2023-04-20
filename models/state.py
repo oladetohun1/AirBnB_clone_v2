c471b0d7e943b706233089b88eac0310e3174bc8
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-20 05:06:05 +0200

    Commit

--- models/state.py
+++ models/state.py
@@ -7,24 +7,19 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-cities = relationship("City", backref="state",
-                      cascade="all, delete")
-
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete")
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    if getenv("HBNB_TYPE_STORAGE", None) != "db":
-        @property
-        def cities(self):
-            """City list"""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    @property
+    def cities(self):
+        """City list"""
+        city_list = []
+        for city in list(models.storage.all(City).values()):
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
