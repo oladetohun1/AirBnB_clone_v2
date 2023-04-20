4a7bb056202a738aa7a916001640c34d0de6c930
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-20 04:47:47 +0200

    Commit

--- models/state.py
+++ models/state.py
@@ -12,15 +12,14 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state",
+    cities = relationship("City", backref="state",
                               cascade="all, delete")
-    else:
-        @property
-        def cities(self):
-            """City list"""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+#    else:
+#        @property
+#        def cities(self):
+#            """City list"""
+#            city_list = []
+#            for city in list(models.storage.all(City).values()):
+#                if city.state_id == self.id:
+#                    city_list.append(city)
+#            return city_list
