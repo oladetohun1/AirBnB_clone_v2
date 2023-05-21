f365f4d8fe56942af6975f339dc2c07fa869b79d
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-21 22:40:37 +0200

    Commit

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     text.replace('_', ' ')
-    return 'C %s' % text
+    return 'C {}'.format(text)
 
 
 if __name__ == '__main__':
