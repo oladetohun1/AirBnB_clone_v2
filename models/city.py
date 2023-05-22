7b2577b9f98d3b7db3e63ef0746940039b26c6d5
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-22 04:05:09 +0200

    Commit

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from os import getenv
 from sqlalchemy.orm import relationship
 from models.state import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
@@ -9,6 +10,7 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name which are mapped columns"""
 
     __tablename__ = "cities"
+
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     places = relationship("Place", backref="cities", cascade="all, delete")
