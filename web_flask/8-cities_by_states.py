b4982471489e11fbbec51c8d3513fe913d1ff32a
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-22 18:27:42 +0200

    Commit

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -16,7 +16,7 @@ def states_list():
     states = storage.all(State).values()
 
     sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    return render_template('8-cities_by_states.html', states=sorted_states)
 
 @app.teardown_appcontext
 def teardown_db():
