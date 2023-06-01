120021f43b340aca970c054c2f6a0fdce7c3d6b3
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-06-01 07:23:11 +0200

    Commit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+"""
+Fabric script generates a .tgz archive from the contents of the web_static
+folder
+"""
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+
+def do_pack():
+    """Generates .tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        filename = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(filename))
+        return filename
+    except Exception:
+        return None
