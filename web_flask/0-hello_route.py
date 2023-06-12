de1d929b169fba9c68545a55185db69bc1f299c5
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-06-12 12:06:59 +0200

    Commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,5 +11,10 @@ def hello_hbnb():
     return 'Hello HBNB!'
 
 
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_route():
+    return 'Hello HBNB!'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
