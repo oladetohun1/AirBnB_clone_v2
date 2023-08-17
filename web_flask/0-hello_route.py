020fb5cef22ce909e2b85004faadec0ad553e632
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-08-17 08:41:52 +0200

    Commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# @app.route('/', strict_slashes=False)
-# def hello_hbnb():
-#    return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
 
 
 @app.route('/airbnb-onepage/', strict_slashes=False)
